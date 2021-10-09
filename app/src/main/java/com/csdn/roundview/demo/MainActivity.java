30295dbc76a934e9fa74d6deba63e531d815bb29
kuanggang <kuanggang@csdn.net>
2021-10-09 17:22:10 +0800

    first commit

---
+++ app/src/main/java/com/csdn/roundview/demo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.csdn.roundview.demo;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.csdn.roundview.RoundTextView;
+
+/**
+ * @author kuanggang on 2019/12/01
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RoundTextView tv = findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setRadius(10, 0, 0, 0);
+                tv.setStrokeWidthColor(5, getResources().getColor(android.R.color.holo_green_dark));
+            }
+        });
+    }
+}
